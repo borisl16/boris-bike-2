401cc0b1af7b6b3ea1fe26dd065ce989b6e05a35
Tom Lee <tomadamlee02@gmail.com>
2021-08-26 16:40:20 +0100

    added raise error for release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,15 +7,23 @@ describe DockingStation do
         bike = Bike.new
         expect(subject.dock(bike)).to eq bike
     end
+
     it 'check if bikes docks' do
         bike = Bike.new
         subject.dock(bike)
         expect(subject.bike). to eq bike
     end
-    it 'releases.working bikes' do
-        bike = subject.release_bike
-        expect(bike).to be_working
-     
-    end
 
+    it 'releases.working bikes' do
+        bike = Bike.new
+        subject.dock(bike)
+        subject.release_bike
+        expect(bike).to be_working
+    end
+    
+    describe '#release_bike' do
+      it 'raises an error when there are no bikes available' do
+        expect { subject.release_bike }.to raise_error 'No bikes available'
+      end
+    end
 end

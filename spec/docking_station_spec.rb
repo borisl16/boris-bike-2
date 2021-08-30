343cd89270aff9dfb0e10cf153e17226bfd563b5
Borislav Lazarov <lazarov_borislav@yahoo.com>
2021-08-30 12:34:19 +0100

    Using Instance Variables

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,12 +2,14 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }  
-
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:bike) }
+  
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
-  
 end
+
   
- 

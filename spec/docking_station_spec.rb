5789c1cdc72124c1ee16a1e86a16279608062eef
Borislav Lazarov <lazarov_borislav@yahoo.com>
2021-08-30 12:47:42 +0100

    Raising Exceptions

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,6 +10,12 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
+
+  describe '#release_bike' do
+      it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
 end
 
   

4d73b1ad879a84e9536d7e81d6d8b3220a8b9f40
Borislav Lazarov <lazarov_borislav@yahoo.com>
2021-08-30 12:03:06 +0100

    Step 8 back to the unit

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,22 +1,5 @@
-require_relative 'bike'
-
 class DockingStation
-  attr_reader :bike
-  
-
-  def initialize 
-   @bike = []
-  end
-
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
-    @bike.pop
-  end
 
-  def dock(bike)
-    fail 'Docking station full' if @bike.count >= 20
-      @bike << bike
   end
-  
-  
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do
-
+  it { is_expected.to respond_to :release_bike }  
+  
 end
   
  

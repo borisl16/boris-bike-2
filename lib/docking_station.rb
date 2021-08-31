acf6a6ea26c8536d9f9700118785199592b56c8f
Borislav Lazarov <lazarov_borislav@yahoo.com>
2021-08-31 16:56:08 +0100

    Wrapping collections

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,17 +2,45 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :bike
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    Bike.new
+    fail 'No bikes available' if @bikes.empty?
+    @bikes.pop
+  end
+
+  def dock(bike)
+    fail 'Docking station full' if @bikes.count >= 20
+    @bikes << bike
+  end
+end
+
+=begin def release_bike
+    @bike
+  end
+
+  def release_bike
+    fail 'No bikes available' unless @bike
+    @bike
   end
 
   def dock(bike)
     @bike = bike
   end
-
-  #def bike
-   # @bike
+  #def dock(bike)
+    #fail 'Docking station full' if @bike
+    #@bike = bike
   #end
 
-end
+  def release_bike
+    fail 'No bikes available' unless @bike
+    @bike
+  end
+
+  def bike
+    @bike
+  end
+
+=end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,16 +2,17 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+ 
+  #it 'releases working bikes' do
+    #bike = subject.release_bike
+    #expect(bike).to be_working
+  #end
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
   it { is_expected.to respond_to(:bike) }
 
-  it 'docks something' do
+=begin it 'docks something' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike 
   end
@@ -20,8 +21,29 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+=end
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+  end
+
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      # Let's not dock a bike first:
+      # remember subject == DockingStation.new
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
+  describe '#dock' do
+    it 'raises an error when full' do
+      20.times {subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error "Docking station full"
+    end
   end
 end
-
-
 

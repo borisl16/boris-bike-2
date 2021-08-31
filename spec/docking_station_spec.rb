b4839c038d13a7e1fdac4ac54311adb8962fe069
Borislav Lazarov <lazarov_borislav@yahoo.com>
2021-08-31 17:43:15 +0100

    magic number

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,17 @@
 require 'docking_station'
 
 describe DockingStation do
+  describe 'dock' do
+    it 'raises an error when full' do
+      subject.capacity.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+end
+
+
+
+=begin describe DockingStation do
   it { is_expected.to respond_to :release_bike }
  
   #it 'releases working bikes' do
@@ -12,7 +23,7 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
   it { is_expected.to respond_to(:bike) }
 
-=begin it 'docks something' do
+#begin it 'docks something' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike 
   end
@@ -21,7 +32,7 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
-=end
+#end
 
   describe '#release_bike' do
     it 'releases a bike' do
@@ -45,5 +56,16 @@ describe DockingStation do
       expect { subject.dock Bike.new }.to raise_error "Docking station full"
     end
   end
-end
+
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+  
+  describe 'dock' do
+    it 'raises an error when full' do
+      subject.capacity.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+=end
 
